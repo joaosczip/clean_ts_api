3fd827ad83ee8a9c02e8373ea7fd64a043166ba5
João Guilherme <joao.sczip@hotmail.com>
2020-03-30 19:09:27 -0300

    feat: ensure SIgnUp route returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import setUpMiddlewares from "../config/middlewares";
+import setUpRoutes from "../config/middlewares";
 
 const app = express();
 setUpMiddlewares(app);
+setUpRoutes(app);
 export default app;
