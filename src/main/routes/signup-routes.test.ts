c9d8ecccacad1c6eda703271ab8aa83cc9d7ea6b
João Guilherme <joao.sczip@hotmail.com>
2020-03-30 21:06:26 -0300

    feat: ensure mongodb reconnect if client is down

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -12,7 +12,7 @@ describe("Sign Up Routes", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection("accounts");
+    const accountCollection = await MongoHelper.getCollection("accounts");
     await accountCollection.deleteMany({});
   });
   it("should return an account on success", async () => {
